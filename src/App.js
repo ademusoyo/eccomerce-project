4e6ce5b85cc62f1c77eb273c2cd8eb033255476f
Ademusoyo Awosika-Olumo <ademusoyo.awosika-olumo@mailchimp.comgit config --global user.name Ademusoyo>
2019-11-25 14:42:13 -0500

    update our menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom'; //Switch renders the first route available. Useful for more control over our code.
 
+const HatsPage = () => (
+  <div>
+    Hats Page
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch> 
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
