625411682b2a83034adf2b7476a88cbb9cb2e6c2
Ademusoyo Awosika-Olumo <ademusoyo.awosika-olumo@mailchimp.comgit config --global user.name Ademusoyo>
2019-11-25 21:03:57 -0500

    add header component.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {Switch, Route} from 'react-router-dom'; //Switch renders the first route
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header';
 
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch> 
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
